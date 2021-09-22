7df85e9f9fc53a1e8f293dbf108d6adc4a51b8d2
MyKirbyVL2016V3 <317783177818266@qq.com>
2021-09-22 13:06:18 +0800

    mykirbyvl2016
    My Websites

---
+++ install me!.bat
@@ -0,0 +1,11 @@
+echo off
+echo Installing MyKirbyVL2016 Animate Websites...
+npm install
+echo go to chromium and edge websites.
+echo done!
+echo DON'T CLOSE WINDOW!
+echo go to websites: soon making...
+start ungoogled-chromium_67.0.3396.87-3_windows\chrome.exe
+npm start
+echo Loading Failed! Please Restart Install 2nd Websites.
+pause
